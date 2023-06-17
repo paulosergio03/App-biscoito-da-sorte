fc8dafdfbc5df0d30a791c2cbfba4bb484acafba
Paulo Sérgio <paulosergioadm9@gmail.com>
2023-06-17 19:03:54 -0300

    Primeiro commit

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component {
+
+
+  render() {
+
+    return (
+      <View style={styles.container}>
+    <Image
+    source={require('./src/biscoito.png')}
+     style={styles.img}
+    />
+
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-});
+  img:{
+    width:250,
+    height: 250,
+  }
+
+})
+
+export default App;
